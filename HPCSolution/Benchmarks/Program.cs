147a128ebed3a4121d12bf3f9508701f3d2410a5
ŁukaszHamera <lucas.hamera@gmail.com>
2019-05-06 23:48:08 +0200

    Restore main in benchmarks project

--- HPCSolution/Benchmarks/Program.cs
+++ HPCSolution/Benchmarks/Program.cs
@@ -1,35 +1,13 @@
-﻿using System;
-using System.Linq;
-using MatrixMultiplication.NormalWrapper;
+﻿using BenchmarkDotNet.Running;
+using System;
 
 namespace Benchmarks
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            var n = 2_500;
-            var nPow = n * n;
-
-            var a = new float[nPow];
-            var b = new float[nPow];
-            var c = new float[nPow];
-
-            for (int i = 0; i < nPow; i += n)
-            {
-                a[i] = 1.0f;
-                b[i] = 1.0f;
-            }
-
-            MatrixMultiplicationWrapper.ParallelMultiply(
-                    a,
-                    b,
-                    c,
-                    n
-                );
-
-
-            //var summary = BenchmarkRunner.Run<MatrixMultiplicationBenchmark>();
+        { 
+            var summary = BenchmarkRunner.Run<MatrixMultiplicationBenchmark>();
 
             Console.ReadKey();
         }
